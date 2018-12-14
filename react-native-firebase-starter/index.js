7af26b1da8c31a17534a25f3a07e8555b8e6a2ef
Your Name <you@example.com>
2018-12-14 23:46:06 +0600

    Start

--- react-native-firebase-starter/index.js
+++ react-native-firebase-starter/index.js
@@ -3,5 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+////////////////////////////////////////////
+import First from './source/First'
+///////////////////////////////////////////
+AppRegistry.registerComponent(appName, () => First);
