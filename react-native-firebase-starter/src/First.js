4dce42fbb9ee2bd1c19c61e72fc3c5004cd8a4b9
Your Name <you@example.com>
2018-12-15 01:39:51 +0600

    Good

---
+++ react-native-firebase-starter/src/First.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class First extends React.Component{
+
+  render()
+  {
+    return(
+      <View>
+        <Text>First Working</Text>
+      </View>
+    )
+  }
+}
+
+export default First;
