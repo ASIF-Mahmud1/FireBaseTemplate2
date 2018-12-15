ae344e845a73f83f6f5289fe6efe0481df5f2784
Your Name <you@example.com>
2018-12-15 16:53:11 +0600

    Tutorial Code.. But I cant see the credentials

--- react-native-firebase-starter/index.js
+++ react-native-firebase-starter/index.js
@@ -3,5 +3,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+/////////////////////////////////////////////////////////////
 import First from './src/First'
-AppRegistry.registerComponent(appName, () => First);
+//import SignIn from './src/SignIn'
+import SignIn from './src/SignIn2'
+////////////////////////////////////////////////////////////
+AppRegistry.registerComponent(appName, () => SignIn);
